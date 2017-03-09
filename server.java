4fe26535239e0916aa2d54cd734df5b3c6d33be8
Aaron <Leekale18@gmail.com>
2017-03-09 17:51:13 +0800

    make the server wait for the client all the time

--- server.java
+++ server.java
@@ -4,17 +4,23 @@ import java.io.*;
 public class server{
 	public static void main(String[] args){
 		ServerSocket server = null;
+		int count=0;
 		try{
 			server= new ServerSocket(4700);		
 		}catch(Exception e){
-			System.out.println("fail");
+			System.out.println("fail and exit");
 		}
+		while(true){
 		Socket socket = null;
+		System.out.println("test");
 		try{
 			socket = server.accept();
-			System.out.println("success");	
+			System.out.println("success");
+			count+=1;
+			System.out.println(count);	
 		}catch(Exception e){
 			System.out.println("can't establish the link");
+			}
 		}
 	}
 }
