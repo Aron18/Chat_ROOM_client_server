370f0453de623157cfc201d4400284b051b712f2
Aaron <Leekale18@gmail.com>
2017-03-11 23:57:31 +0800

    finish the simple server function

--- server.java
+++ server.java
@@ -1,27 +1,70 @@
-import java.net.*;
 import java.io.*;
+import java.net.*;
+import java.util.*;
 
 public class server{
-	public static void main(String[] args){
-		ServerSocket server = null;
-		int count=0;
-		try{
-			server= new ServerSocket(4700);		
-		}catch(Exception e){
-			System.out.println("fail and exit");
-			System.exit(0);
-		}
-		while(true){
-		Socket socket = null;
-		System.out.println("test");
-		try{
-			socket = server.accept();
-			System.out.println("success");
-			count+=1;
-			System.out.println(count);	
-		}catch(Exception e){
-			System.out.println("can't establish the link");
+	ArrayList clientOutputStreams;
+
+	public class ClientHandler implements Runnable{
+		BufferedReader reader;
+		Socket sock;
+
+		public ClientHandler(Socket clientSocket){
+			try{
+				sock = clientSocket;
+				InputStreamReader isReader = new InputStreamReader(sock.getInputStream());
+				reader = new BufferedReader(isReader);
+			}catch(Exception ex){
+				ex.printStackTrace();
 			}
 		}
+
+		public void run(){
+			String message;
+			try{
+				while((message = reader.readLine()) != null){
+					System.out.println("read " + message);
+					tellEveryone(message);
+					}
+				}catch(Exception ex){
+				ex.printStackTrace();
+				}
+			}
+		}
+
+	public void go(){
+		clientOutputStreams = new ArrayList();
+		try{
+			ServerSocket serverSock = new ServerSocket(4700);
+
+			while(true){
+				Socket clientSocket = serverSock.accept();
+				PrintWriter writer = new PrintWriter(clientSocket.getOutputStream());
+				clientOutputStreams.add(writer);
+
+				Thread t = new Thread(new ClientHandler(clientSocket));
+				t.start();
+				System.out.println("got a connection");
+			}
+		}catch(Exception ex){
+			ex.printStackTrace();
+		}
+	}
+
+	public void tellEveryone(String message){
+		Iterator it = clientOutputStreams.iterator();
+		while(it.hasNext()){
+			try{
+				PrintWriter writer = (PrintWriter)it.next();
+				writer.println(message);
+				writer.flush();
+			}catch(Exception ex){
+				ex.printStackTrace();
+			}
+		}
+	}
+
+	public static void main(String[] args){
+		new server().go();
 	}
 }
