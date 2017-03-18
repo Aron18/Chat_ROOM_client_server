cfd94c3b9e45622074fbcd9850dfe1bb15f2d90d
Aaron <Leekale18@gmail.com>
2017-03-19 00:33:36 +0800

    rebuild the server

--- server.java
+++ server.java
@@ -1,99 +1,48 @@
 import java.io.*;
 import java.net.*;
 import java.util.*;
-import java.io.ObjectInputStream;  
-import com.Message;
+import java.awt.event.*;
 
 public class server{
-	ArrayList clientOutputStreams;
-	ArrayList outputname;
-	ObjectOutputStream oos;
-	PrintWriter helloname;
-	PrintWriter writer;
-
-	public class ClientHandler implements Runnable{
-		BufferedReader reader;
-		Socket sock;
-		ObjectInputStream ois;
-
-		public ClientHandler(Socket clientSocket){
-			try{
-				sock = clientSocket;
-				ois = new ObjectInputStream(new BufferedInputStream(sock.getInputStream()));
-				//Object obj = ois.readObject();  //get the object sent bt client
-				//InputStreamReader isReader = new InputStreamReader(sock.getInputStream());
-				//reader = new BufferedReader(isReader);
-			}catch(Exception ex){
-				ex.printStackTrace();
-			}
-		}
-
-		public void run(){
-			String name;
-			String message;
-			try{
-				System.out.println("test1");
-				Object obj = ois.readObject();   
-                Message user = (Message)obj;
-                System.out.println("test2");
-                if(user.getType() == "User_name"){
-                	name = user.getName();
-                	System.out.println("user: " + user.getName());
-                	Hello(name);    
-                }
-                System.out.println("test3"); 
-				/*while((message = reader.readLine()) != null){
-					System.out.println("read " + message);
-					Send(message);
-					}*/
-				}catch(Exception ex){
-					ex.printStackTrace();
-				}
-			}
-		}
-
+	
 	public void go(){
-		clientOutputStreams = new ArrayList();
-		outputname = new ArrayList();
+		ServerSocket serverSock = new ServerSocket(5000);
 		try{
-			ServerSocket serverSock = new ServerSocket(5000);
-
 			while(true){
-				Socket clientSocket = serverSock.accept();
-				helloname = new PrintWriter(clientSocket.getOutputStream());
-				writer = new PrintWriter(clientSocket.getOutputStream());
-				clientOutputStreams.add(writer);
-				System.out.println("test");
-
-				Thread t = new Thread(new ClientHandler(clientSocket));	//wait for new user
+				Socket socket = serverSock.accept();
+				Thread t = new Thread(new ServerProcess(socket));
 				t.start();
 				System.out.println("got a connection");
 			}
 		}catch(Exception ex){
-			ex.printStackTrace();
-		}
+				System.out.println("can't not find a connection");
+				ex.printStackTrace();
+			}
 	}
 
-	public void Send(String message){
-		Iterator it = clientOutputStreams.iterator();
-		while(it.hasNext()){
-			try{
-				writer = (PrintWriter)it.next();
-				writer.println(message);
-				writer.flush();
-			}catch(Exception ex){
-				ex.printStackTrace();
+	public class ServerProcess implements Runnable{
+		Socket socket;
+		BufferedReader in;
+		PrintWriter out;
+
+		public ServerProcess(Socket client){
+			socket = client;
+
+			in = new BufferedReader(new InputStreamReader(socket.getInputStream()));	//get the message
+			out = new PrintWriter(new BufferedReader(new OutputStreamWriter(socket.getOutputStream())));	//send the message
+
+			public void run(){
+				try{
+					while(true){
+
+					}
+				}cathc(Exception ex){
+					ex.printStackTrace();
+				}
 			}
 		}
 	}
-
-	public void Hello(String message){
-		System.out.println("Hello " + message);
-		writer.println("Hello " + message);
-		writer.flush();
-	}
-
-	public static void main(String[] args){
+	public static void mian(String[] args){
 		new server().go();
 	}
 }
