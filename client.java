5f4bfe07387a0a335d3222e447748c6c3645a089
Aaron <Leekale18@gmail.com>
2017-03-19 22:42:19 +0800

    Done,finish the simple function

--- client.java
+++ client.java
@@ -1,134 +1,154 @@
 import java.io.*;
-import java.io.ObjectOutputStream; 
+import java.io.ObjectOutputStream;
 import java.net.*;
 import java.util.*;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 
-public class client{
-	String name,username;
-	JTextField usrname;
-	JFrame frame1;	//login panel
-	JFrame frame2;	//chat panel
-	BufferedReader in;
-	PrintWriter out;
-	Socket socket;
-	JButton loginButtion;
-	JButton sendButton;
-	JTextArea inputarea,chatpanel;
+public class client {
+    String name, username;
+    JTextField usrname;
+    JFrame frame1;    //login panel
+    JFrame frame2;    //chat panel
+    BufferedReader in;
+    PrintWriter out;
+    Socket socket;
+    JButton loginButtion;
+    JButton sendButton;
+    JTextArea inputarea, chatpanel;
 
-	private void setUpNetworking(){		
-		try{
-			socket = new Socket("127.0.0.1",5000);
-			in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-			out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(socket.getOutputStream())));
+    private void setUpNetworking() {
+        try {
+            socket = new Socket("127.0.0.1", 5000);
+            in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(socket.getOutputStream())));
 
-			//Thread t = new Thread(new inputListener());
-			//t.start();
-			//System.out.println("networking established");
+            Thread t = new Thread(new inputListener(socket));
+            t.start();
+            System.out.println("networking established");
 
-			}catch(IOException ex){
-				System.out.println("Fail");
-				ex.printStackTrace();
-		}
-	}
-	
-	public void login(){
-		setUpNetworking();
-		frame1 = new JFrame("login in panel");
-		JPanel logininPanel =  new JPanel();
-		JLabel label1=new JLabel("UR NAME"); 
-		usrname = new JTextField(20);
-		loginButtion = new JButton("login");
-		JScrollPane qScroller = new JScrollPane(usrname);
-		qScroller.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
-		qScroller.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
-		loginButtion.addActionListener(new loginButtionListener());
-		logininPanel.add(qScroller);
-		logininPanel.add(label1);
-		logininPanel.add(usrname);
-		logininPanel.add(loginButtion);
-		setUpNetworking();
+        } catch (IOException ex) {
+            System.out.println("Fail");
+            ex.printStackTrace();
+        }
+    }
 
-		frame1.getContentPane().add(BorderLayout.CENTER,logininPanel);
-		frame1.setSize(400,500);
-		frame1.setVisible(true);	//show the login panel
-	}
+    public void login() {
+        setUpNetworking();
+        frame1 = new JFrame("login in panel");
+        JPanel logininPanel = new JPanel();
+        JLabel label1 = new JLabel("UR NAME");
+        usrname = new JTextField(20);
+        loginButtion = new JButton("login");
+        JScrollPane qScroller = new JScrollPane(usrname);
+        qScroller.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+        qScroller.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+        loginButtion.addActionListener(new loginButtionListener());
+        logininPanel.add(qScroller);
+        logininPanel.add(label1);
+        logininPanel.add(usrname);
+        logininPanel.add(loginButtion);
+//        setUpNetworking();
 
-	public void chatPane(){
-		frame2 = new JFrame("Chat Room");
-		JPanel chatP = new JPanel();
-		chatpanel = new JTextArea(10,30);	//show the chat history
-		inputarea = new JTextArea(5,20);	//the area to input the message
-		sendButton = new JButton("Send");
-		
-		sendButton.addActionListener(new sendButtonListener());
-		chatpanel.setLineWrap(true);	//change lines
-		chatpanel.setEditable(false);	//don't allow to edit
-		
-		chatP.add(chatpanel);
-		chatP.add(inputarea);
-		chatP.add(sendButton);
+        frame1.getContentPane().add(BorderLayout.CENTER, logininPanel);
+        frame1.setSize(400, 500);
+        frame1.setVisible(true);    //show the login panel
+    }
 
-		frame2.getContentPane().add(BorderLayout.CENTER,chatP);
-		frame2.setSize(400,600);
-		frame2.setVisible(false);
-	}
+    public void chatPane() {
+        frame2 = new JFrame("Chat Room");
+        JPanel chatP = new JPanel();
+        chatpanel = new JTextArea(10, 30);    //show the chat history
+        inputarea = new JTextArea(5, 20);    //the area to input the message
+        sendButton = new JButton("Send");
 
-	public static void main(String[] args){
-		client Client = new client();
-		Client.login();
-		Client.chatPane();
-	}
+        sendButton.addActionListener(new sendButtonListener());
+        chatpanel.setLineWrap(true);    //change lines
+        chatpanel.setEditable(false);    //don't allow to edit
 
-	public class loginButtionListener implements ActionListener{
-			public void actionPerformed(ActionEvent ev){
-				Object obj = ev.getSource();	//choose a button to react
-				try{
-					if(obj.equals(loginButtion))
-					{
-						System.out.println("test");
-						if(usrname.getText().length() > 0){
-							name = "user_name|" + usrname.getText();
-							username = usrname.getText();
-							System.out.println(name);
-							out.println(name);
-							out.flush();
-						}
-						frame1.setVisible(false);
-						frame2.setVisible(true);
-					}
-				}catch(Exception e){
-					e.printStackTrace();
-					System.out.println("U R offline");
-				}
-			}
-	}
+        chatP.add(chatpanel);
+        chatP.add(inputarea);
+        chatP.add(sendButton);
 
-	public class sendButtonListener implements ActionListener{
-		public void actionPerformed(ActionEvent ev){
-			Object obj = ev.getSource();
-			String message;
-			try{
-				if(obj.equals(sendButton)){
-					if(inputarea.getText().length() > 0){
-						//System.out.println(inputarea.getText());
-						message = "Message|" + inputarea.getText() + "|" + username;
-						System.out.println(message);
-						out.println(message);
-						out.flush();
-						inputarea.setText("");
-					}
-				}
-			}catch(Exception e){
-				e.printStackTrace();
-				System.out.println("User is offline");
-			}
-		}
-	}
+        frame2.getContentPane().add(BorderLayout.CENTER, chatP);
+        frame2.setSize(400, 600);
+        frame2.setVisible(false);
+    }
 
-	/*public inputListener implements Runnable{
-			
-	}*/
+    public static void main(String[] args) {
+        client Client = new client();
+        Client.login();
+        Client.chatPane();
+    }
+
+    public class loginButtionListener implements ActionListener {
+        public void actionPerformed(ActionEvent ev) {
+            Object obj = ev.getSource();    //choose a button to react
+            try {
+                if (obj.equals(loginButtion)) {
+                    System.out.println("test");
+                    if (usrname.getText().length() > 0) {
+                        name = "user_name|" + usrname.getText();
+                        username = usrname.getText();
+                        System.out.println(name);
+                        out.println(name);
+                        out.flush();
+                    }
+                    frame1.setVisible(false);
+                    frame2.setVisible(true);
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+                System.out.println("U R offline");
+            }
+        }
+    }
+
+    public class sendButtonListener implements ActionListener {
+        public void actionPerformed(ActionEvent ev) {
+            Object obj = ev.getSource();
+            String message;
+            try {
+                if (obj.equals(sendButton)) {
+                    if (inputarea.getText().length() > 0) {
+                        //System.out.println(inputarea.getText());
+                        message = "Message|" + inputarea.getText() + "|" + username;
+                        // System.out.println(message);
+                        out.println(message);
+                        out.flush();
+                        inputarea.setText("");
+                    }
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+                System.out.println("User is offline");
+            }
+        }
+    }
+
+    public class inputListener implements Runnable {
+        Socket socket;
+        BufferedReader reader;
+
+        public inputListener(Socket socket) throws IOException {
+            this.socket = socket;
+            reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        }
+
+        public void run() {
+            String mea = "";
+            while (true) {
+                try {
+                    mea = reader.readLine();
+                    if (mea != null) {
+                        System.out.println(mea);
+                        chatpanel.append(mea + "\n");
+                    }
+                } catch (Exception ex) {
+                    ex.printStackTrace();
+                }
+            }
+        }
+    }
 }
--- server.java
+++ server.java
@@ -3,96 +3,100 @@ import java.net.*;
 import java.util.*;
 import java.awt.event.*;
 
-public class server{
-	PrintWriter out;
-	String receive,worked;
-	Socket socket;
-	StringTokenizer	token;
-	Set<Socket> hashset;
+public class server {
+    PrintWriter out;
+    String receive, worked;
 
-	public class ServerProcess implements Runnable{
-		BufferedReader in;
-		PrintWriter out;
+    StringTokenizer token;
+    Set<Socket> hashset;
 
-		public ServerProcess(Socket clientSocket) throws IOException{
-			try{
-				socket = clientSocket;
-				in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-				out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(socket.getOutputStream())));
-			}catch(Exception ex){
-				ex.printStackTrace();
-			}
-		}
+    public class ServerProcess implements Runnable {
+        BufferedReader in;
+        PrintWriter out;
+        Socket socket;
 
-		public void run(){
-			try{
-				while(true){
-					receive = in.readLine();
-					//System.out.println(receive);
-					token = new StringTokenizer(receive,"|");	//process the message
-					worked = token.nextToken();	//Returns the next token from this string tokenizer
-					if(worked.equals("user_name")){
-						String name1 = token.nextToken();
-						hello(name1);
-					}
-					else if (worked.equals("Message")){
-						String mes = token.nextToken();	//get the message
-						String uer = token.nextToken();	//get the user
-						String messs = uer + " : " + mes;  
-						chat(messs);
-					}
-				}
-			}catch(Exception ex){
-				ex.printStackTrace();
-				}
-			}
-		}
 
-	private void hello(String name){
+        public ServerProcess(Socket clientSocket) throws IOException {
+            try {
+                socket = clientSocket;
+                in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(socket.getOutputStream())));
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+        }
 
-		for(Socket s : hashset){
-			try{
-				PrintWriter psend = new PrintWriter(new BufferedWriter(new OutputStreamWriter(socket.getOutputStream())));
-				//String name = token.nextToken();	//get the user name
-				psend.println("!!!"+ " " + name + " is online now" + "!!!" );
-				psend.flush();
-				System.out.println("!!!"+ " " + name + " is online now");
-				}catch(Exception ex){
-				ex.printStackTrace();
-				}
-		}
-	}
-	private void chat(String mess){
-		//String message;
-		for(Socket s : hashset){
-				try{
-					PrintWriter psed = new PrintWriter(new BufferedWriter(new OutputStreamWriter(socket.getOutputStream())));
-					psed.println(mess);
-					System.out.println(mess);
-				}catch(Exception ex){
-					ex.printStackTrace();
-				}
-			}
-		}
+        public void run() {
+            try {
+                while (true) {
+                    receive = in.readLine();
+                    //System.out.println(receive);
+                    token = new StringTokenizer(receive, "|");    //process the message
+                    worked = token.nextToken();    //Returns the next token from this string tokenizer
+                    if (worked.equals("user_name")) {
+                        String name1 = token.nextToken();
+                        hello(name1);
+                    } else if (worked.equals("Message")) {
+                        String mes = token.nextToken();    //get the message
+                        String uer = token.nextToken();    //get the user
+                        String messs = uer + " : " + mes;
+                        chat(messs);
+                    }
+                }
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+        }
+    }
 
-	public void go(){
-		try{
-			ServerSocket serverSock = new ServerSocket(5000);
-			hashset = new HashSet<>();
-			while(true){
-				socket = serverSock.accept();
-				hashset.add(socket); //
-				Thread t = new Thread(new ServerProcess(socket));
-				t.start();
-				System.out.println("get a connection");
-			}
-		}catch(IOException E){
-			E.printStackTrace();
-			System.out.println("fail to connect");
-		}
-	}
+    private void hello(String name) {
 
-	public static void main(String[] args){
-		new server().go();
-	}
+        for (Socket s : hashset) {
+            try {
+                PrintWriter psend = new PrintWriter(new BufferedWriter(new OutputStreamWriter(s.getOutputStream())));
+                //String name = token.nextToken();	//get the user name
+                psend.println("!!!" + " " + name + " is online now" + "!!!");
+                psend.flush();
+                System.out.println("!!!" + " " + name + " is online now");
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+        }
+    }
+
+    private void chat(String mess) {
+        //String message;
+        for (Socket s : hashset) {
+            try {
+                PrintWriter psed = new PrintWriter((s.getOutputStream()));
+                psed.println(mess);
+                psed.flush();
+                System.out.println(mess);
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+        }
+    }
+
+    public void go() {
+        try {
+            ServerSocket serverSock = new ServerSocket(5000);
+            hashset = new HashSet<>();
+            while (true) {
+                Socket socket;
+                socket = serverSock.accept();
+                hashset.add(socket); //
+                Thread t = new Thread(new ServerProcess(socket));
+                t.start();
+                System.out.println("get a connection");
+            }
+        } catch (IOException E) {
+            E.printStackTrace();
+            System.out.println("fail to connect");
+        }
+    }
+
+    public static void main(String[] args) {
+        new server().go();
+    }
 }
